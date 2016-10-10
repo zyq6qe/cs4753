931fa9cada3b6b6c97cae5656580be284f26dc17
Zophie Quan <zyq6qe@virginia.edu>
2016-10-09 20:09:19 -0400

    signup

--- signup.php
+++ signup.php
@@ -1,46 +1,18 @@
 <?php
 require "config.php";
 
-$firstnameErr = $lastnameErr = $emailErr = $addrErr = $cityErr = $stateErr = $zipErr = "";
-$firstname = $lastname = $email = $addr = $city = $state = $zip = "";
+$firstname = $lastname = $email = $addr = $city = $state = $zip = '';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["firstname"])) {
-        $firstnameErr = "First name is required";
-    } else {
-        $firstname = $_POST["firstname"];
-    }
-    if (empty($_POST["lastname"])) {
-        $lastnameErr = "Last name is required";
-    } else {
-        $lastname = $_POST["lastname"];
-    }
-    if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-    } else {
-        $email = $_POST["email"];
-    }
-    if (empty($_POST["address"])) {
-        $addrErr = "Address is required";
-    } else {
-        $addr = $_POST["address"];
-    }
-    if (empty($_POST["city"])) {
-        $cityErr = "City is required";
-    } else {
-        $city = $_POST["city"];
-    }
-    if (empty($_POST["state"])) {
-        $stateErr = "State is required";
-    } else {
-        $state = $_POST["state"];
-    }
-    if (empty($_POST["zipcode"])) {
-        $zipErr = "Zip code is required";
-    } else {
-        $zip = $_POST["zipcode"];
-    }
-}
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$addr = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = $_POST['zipcode'];
 
-$db->query("insert into Users values('$email', '$firstname', '$lastname', '$addr', '$city', '$state', '$zip')") or die ("Invalid query ".$db->error);
+$db->query("insert into Users values('$email', '$firstname', '$lastname', '$addr', '$city', '$state', '$zip')") or die ('Invalid query '.$db->error);
 ?>
+
+You have been registered!
+<p></p>
