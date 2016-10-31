5a9ba66855218b81bf59ca5f2a9c7cf43e059536
Zophie Quan <zyq6qe@virginia.edu>
2016-10-30 22:14:37 -0400

    setup for tables and signup backend

--- setup.php
+++ setup.php
@@ -3,4 +3,8 @@
 require "config.php";
 
 $db->query("drop table Users");
-$db->query("create table Users (email char(255) primary key, firstName char(255) not null, lastName char(255) not null, address char(255) not null, city char(255) not null, state char(255) not null, zipCode int not null)") or die ("users ".$db->error);
+$db->query("drop table Payment");
+
+$db->query("create table Users (email char(255) UNIQUE NOT NULL, firstName char(255) NOT NULL, lastName char(255) NOT NULL, address char(255) NOT NULL, city char(255) NOT NULL, state char(255) NOT NULL, zipCode int NOT NULL, PRIMARY KEY (email))") or die ("users ".$db->error);
+$db->query("create table Payment (email char(255) NOT NULL, card_num int NOT NULL, exp_month int NOT NULL, exp_year int NOT NULL, cvv int NOT NULL, FOREIGN KEY (email) REFERENCES Users(email))") or die ("users ".$db->error);
+
--- signupConfirm.php
+++ signupConfirm.php
@@ -10,6 +10,7 @@ include "base.php";
 require "config.php";
 
 $firstname = $lastname = $email = $password = $addr = $city = $state = $zip = '';
+$cardnum = $expmonth = $expyear = $cvv = '';
 $feedback = '';
 
 if (isset($_POST['firstname'])) {
@@ -21,10 +22,15 @@ if (isset($_POST['firstname'])) {
     $city = $_POST['city'];
     $state = $_POST['state'];
     $zip = $_POST['zipcode'];
+    $cardnum = $_POST['cardnum'];
+    $expmonth = $_POST['expmonth'];
+    $expyear = $_POST['expyear'];
+    $cvv = $_POST['cvv'];
 }
 
 try {
     $db->query("insert into Users values('$email', '$password', '$firstname', '$lastname', '$addr', '$city', '$state', '$zip')");
+    $db->query("insert into Payment values('$email', '$cardnum', '$expmonth', '$expyear', '$cvv')");
     $feedback = 'Thanks for registering!';
 } catch (Exception $e) {
     $feedback = 'Error in registration. Please try again!';
